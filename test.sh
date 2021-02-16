85e2f48d60675b53863b54d3cc3258e353af2b8d
Benjamin Baier <programmer@netzbasis.de>
2021-02-16 12:53:32 +0100

    tweak tests

--- test.sh
+++ test.sh
@@ -10,17 +10,32 @@ function sndtest {
 }
 
 echo "Starting tests"
+echo "White noise -> left -> right"
+
 sndtest ""
 sndtest "-rl" #ignore r
 sndtest "-lr" #ignore l
+
+echo "Sine -> left -> right"
+
 sndtest "-c -s 150" #ignore c
 sndtest "-s 250 -l"
 sndtest "-s 350 -r"
+
+echo "Chirp -> left -> right"
+
 sndtest "-c"
 sndtest "-s 1000 -c" #ignore s
 sndtest "-s 1000 -c -rl" #ignore sr
 sndtest "-s 1000 -c -lr" #ignore sl
-sndtest "-d 1 -l" 4
+
+echo "White noise delay -> left -> right"
+
+sndtest "-d 2" 6
+sndtest "-d 1 -l" 6
 sndtest "-d 2 -r" 6
+
+echo "Buffer dump to ./obj/"
+
 $CMD -s500 -r -o > obj/s16nat-44.1k-2ch.pcm
 $CMD -c -o > obj/chirp.pcm
